51b99bb7ae8c1a487b15d17e02b93de0865e8e69
Martin <martin.funkquist@gmail.com>
2018-02-21 23:13:42 +0100

    Added react router and styled components

--- App/App.js
+++ App/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link } from 'react-router-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <NativeRouter>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+
+        {/* <Route exact path="/" component={} /> */}
+
+      </NativeRouter>
     );
   }
 }
